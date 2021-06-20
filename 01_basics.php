1ef46f8bf88747294f3eee30995d7cd689b7424b
vijayvj777 <80820027+vijayvj777@users.noreply.github.com>
2021-06-20 15:18:51 +0530

    Add files via upload

---
+++ 01_basics.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php project</title>
+</head>
+<body>
+    <div class="container">
+    This is my first php site.
+    <?php
+        echo "hello world<br>";
+        $variable1 =34;
+        $variable2 =45;
+        echo $variable1;
+        echo "<br>";
+        echo $variable2;
+        echo "<br>";
+        echo $variable1 + $variable2;
+    ?>
+    </div>
+</body>
+</html>
---
+++ 02_bsaics.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>basicsss</title>
+    <style>
+    *{
+        margin: 0;
+        padding:  0;
+        box-sizing: border-box;
+    }
+    .container{
+        max-width: 80%;
+        background-color: rgb(104, 95, 95);
+        margin: auto;
+        padding: 23px;
+
+    }
+    </style>
+</head>
+<body>
+<div class="container">
+<h1>Lets Learm About Php</h1>
+<?php
+$age =34;
+if($age>18){
+    echo"you can go to the party";
+}
+
+?>
+
+</div>
+    
+</body>
+</html>
